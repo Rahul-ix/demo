14e1355564d21a921eb9c9b286bd7bbadfa780d7
rahul <rahulkalal676@gmail.com>
2025-04-01 16:58:27 +0530

    changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+boolean=true;
+String s="name";
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
