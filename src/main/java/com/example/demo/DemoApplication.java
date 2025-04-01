b2576d127ed93b56a60218370330c7eb77f48bdb
rahul <rahulkalal676@gmail.com>
2025-04-01 17:16:07 +0530

    changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,8 +15,18 @@ int x=10;
 	int y=20;
 	int z=0; 
 
+
         int x=10;
 	int y=20;
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
 	int z=0;   
 int x=10;
 	int y=20;
@@ -27,6 +37,7 @@ int x=10;
 int x=10;
 	int y=20;
 	int z=0; 
+
     
        boolean=true;
        String s="name";
