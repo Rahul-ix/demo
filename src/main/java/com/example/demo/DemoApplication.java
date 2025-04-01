83fb221270c1e3c9099297bc632667d11ec39bc9
Rahul <53251733+Rahul-ix@users.noreply.github.com>
2025-04-01 16:57:06 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	int x=10;
+	int y=20;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
